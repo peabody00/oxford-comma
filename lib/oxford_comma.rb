2258aac300e5ec9335eef6ee86f9d635a3fea884
Charlie <peabody00@gmail.com>
2020-11-19 13:35:16 -0600

    commit tree a6a7b9368d6a10b0cc52c0d27f752cbe2aaad846

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.count <= 2
+        array.join(" and ")
+    else array.count >= 3
+        last = array.pop
+        array.push("and " + last)
+        array.join(", ")        
+        end
 end
